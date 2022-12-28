93306f2ef8a318d684df32af81503841f61aeab9
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-12-28 15:49:16 +0900

    observable , observer study

--- app/src/main/java/com/example/rxproject/MainActivity.kt
+++ app/src/main/java/com/example/rxproject/MainActivity.kt
@@ -2,10 +2,60 @@ package com.example.rxproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 
 class MainActivity : AppCompatActivity() {
+
+    private var greeting : String = "Hello From RxJava"
+
+    private lateinit var observable : Observable<String>
+
+    private lateinit var myObserver : Observer<String>
+
+    companion object var TAG : String = "my APP"
+
+    private lateinit var txt : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        observable = Observable.just(greeting)
+
+        txt = findViewById(R.id.textview)
+
+        myObserver = object  : Observer<String> {
+            override fun onSubscribe(d: Disposable) {
+
+                Log.i(TAG , "onSubscribe invoked")
+
+            }
+
+            override fun onNext(t: String) {
+                Log.i(TAG , "onNext invoked")
+                txt.text = t
+
+
+            }
+
+            override fun onError(e: Throwable) {
+                Log.i(TAG , "onError invoked")
+
+            }
+
+            override fun onComplete() {
+                Log.i(TAG , "onComplete invoked")
+
+            }
+
+        }
+
+        observable.subscribe(myObserver)
+
+
     }
 }
